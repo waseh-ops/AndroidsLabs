725ea74c40f743afcf53a0bae71751f5d1ffdcc7
Waseh <muhammad.waseh2013@gmail.com>
2020-10-10 13:58:15 -0400

    lab 3 commit

--- app/src/main/java/com/example/androidslabs/MainActivity.java
+++ app/src/main/java/com/example/androidslabs/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.androidslabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,53 +17,47 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    Switch btn1;
+    private EditText emailET;
+    private EditText passET;
+    private Button loginBtn;
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    public final static String SP_NAME = "SharePreference_File";
+    public final static String EMAIL_SP_KEY = "email_sp_key";
+    public final static String EMAIL_INTENT_KEY = "EMAIL_INTENT_KEY";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
 
+        emailET = findViewById(R.id.emailInput);
+        passET = findViewById(R.id.passInput);
+        sp = getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+        editor = sp.edit();
 
-        btn = findViewById(R.id.button3);
-        btn.setOnClickListener(v -> {
+        emailET.setText(sp.getString(EMAIL_SP_KEY,""));
 
-            Toast.makeText(MainActivity.this, this.getString(R.string.toast_message), Toast.LENGTH_LONG ).show();
-
-
-        });
-
-        btn1 = findViewById(R.id.switch1);
-
-        btn1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-
-            public void onCheckedChanged (CompoundButton cb, boolean b){
-               if(b == true){
-                   Snackbar.make(btn1, (R.string.switch_on), Snackbar.LENGTH_SHORT)
-                           .setAction((R.string.undo), click -> cb.setChecked(!b))
-                           .show();
-
-               }else{
-                   //snackbar.make(btn1, "The Switch is now Off, Snackbar.LENGTH_SHORT).show(); if i do this it doesn't show up in french
-                   //thats why i did the following
-                   Snackbar.make(btn1, (R.string.switch_off), Snackbar.LENGTH_SHORT)
-                           .setAction((R.string.undo), click -> cb.setChecked(!b))
-                           .show();
-               }
-
+        loginBtn= findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToProfile = new Intent(MainActivity.this,ProfileActivity.class);
+                goToProfile.putExtra(EMAIL_INTENT_KEY,emailET.getText().toString());
+                startActivity(goToProfile);
             }
-
-            
         });
-
-
-
-
-
-
-
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String emailAddr = emailET.getText().toString();
+        editor.putString(EMAIL_SP_KEY,emailAddr);
+        editor.commit();
+//        emailET.setText("");
 
+    }
 }
---
+++ app/src/main/java/com/example/androidslabs/ProfileActivity.java
@@ -0,0 +1,77 @@
+package com.example.androidslabs;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton imgBtn;
+    EditText emailET;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+//        sp = getSharedPreferences(MainActivity.SP_NAME, Context.MODE_PRIVATE);
+        Intent mainActiivty = getIntent();
+        String email = mainActiivty.getStringExtra(MainActivity.EMAIL_INTENT_KEY);
+        emailET=findViewById(R.id.emailInputProfile);
+        emailET.setText(email);
+
+        imgBtn = findViewById(R.id.takePictureBtn);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakePictureIntent();
+
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.e("ProfileActivity", "In function:" +"onActivityResult");
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgBtn.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e("ProfileActivity", "In function:" +"onActivityResult");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
